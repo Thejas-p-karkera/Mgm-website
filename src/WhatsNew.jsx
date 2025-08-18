3b4f702964641ab2cff1a6a40ef4f45f7ad0900a
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2025-08-18 09:18:03 +0530

    Add files via upload

---
+++ src/WhatsNew.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import HeaderBar from './WhatsNew-Divya/HeaderBar'
+import NavBar from './WhatsNew-Divya/NavBar'
+import ArticleCard from './WhatsNew-Divya/ArticleCard'   
+import Footer from './Footer'
+import Header from './Header'
+
+
+
+function WhatsNew() {
+  
+
+  return (
+    <>
+    {/* <Header/>*/}
+      <HeaderBar/>
+      <NavBar/>
+      <ArticleCard/>
+     {/*<Footer/>*/}
+    </>
+  )
+}
+
+export default WhatsNew
